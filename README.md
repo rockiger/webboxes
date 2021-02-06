85fc28ca9d0ced235bc64f6aba872ea477aec406
Marco Laspe <marco@rockiger.com>
2021-02-06 23:13:51 +0100

    Install basic box

--- README.md
+++ README.md
@@ -4,108 +4,20 @@ Eeasy as pie Bitnami Installers for local web develoment.
 
 Install and administer any Bitnami installer from one app.
 
-- [ ] Get Name of Box / Blog name
-- [ ] Install bitnami
-- [ ] Do Customization
+- [x] Get Name of Box / Blog name
+- [x] Install bitnami
+- [x] Do Customization
+- [x] Show boxes
+- [ ] Start/Stop boxes
+- [ ] open browser window to box
+- [ ] open browser window to phpMyAdmin
+- [ ] open app directory
+- [ ] sanitize box name
+- [ ] Show spinner during installation
+- [ ] Show toast when finished
+- [ ] Download bitnami on first start
 - [ ] Phpinfo
 - [ ] Starter Information before first box creation
-
-Bitnami WordPress Stack 5.6-0
-Usage:
-
---help Display the list of valid options
-
---version Display product information
-
---unattendedmodeui none Unattended Mode UI
-Default: none
-Allowed: none minimal minimalWithDialogs
-
---optionfile <optionfile> Installation option file
-Default:
-
---debuglevel <debuglevel> Debug information level of verbosity
-Default: 2
-Allowed: 0 1 2 3 4
-
---mode unattended Installation mode
-Default: qt
-Allowed: qt gtk xwindow text unattended
-
---debugtrace <debugtrace> Debug filename
-Default:
-
---disable-components varnish Comma-separated list of components
-Default:
-Allowed: varnish phpmyadmin
-
---prefix path/to/installdir Select a folder
-Default:
-
---base_user_name <user_name> Your real name
-Default: User Name
-
---base_mail <base_mail> Email Address
-Default: user@example.com
-
---base_user <base_user> Login
-Default: user
-
---base_password <base_password> Password
-Default:
-
---apache_server_port 8081.... Apache Web Server Port
-Default: 80 or 8080 - depending on your permissions
-
---apache_server_ssl_port <apache_server_ssl_port> SSL Port
-Default: 443 or 8443 - depending on your permissions
-
---mysql_port <mysql_port> 3306 MySQL Server port
-Default: 3306
-
---mysql_password <mysql_password> MySQL Server root password
-Default:
-
---mysql_database_name <mysql_database_name> DB Name
-Default: mysql_db
-
---mysql_database_username <mysql_database_username> DB User Name
-Default: mysql_user
-
---mysql_database_password <mysql_database_password> DB User Password
-Default:
-
---wordpress_blog_name <wordpress_blog_name> Blog name
-Default: user's Blog!
-
---wordpress_site <wordpress_site> Hostname
-Default: i7590
-
---smtp_email_provider <smtp_email_provider> Default email provider:
-Default: custom
-Allowed: gmail custom
-
---smtp_enable <smtp_enable> Do you want to configure mail support?
-Default: 0
-
---smtp_user <smtp_user> Username
-Default:
-
---smtp_password <smtp_password> Password
-Default:
-
---smtp_host <smtp_host> SMTP Host
-Default:
-
---smtp_port <smtp_port> SMTP Port
-Default: 587
-
---smtp_protocol <smtp_protocol> Secure connection
-Default: tls
-Allowed: none ssl tls
-
---phpmyadmin_password <phpmyadmin_password>  
- Default:
-
---launch_cloud 0 Launch wordpress in the cloud with Bitnami
-Default: 1
+- [ ] Add more types of boxes
+- [ ] Remove bitnami banner
+- [ ] Remove box
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,8 @@
-import * as React from 'react'
+import React from 'react'
+import child_process from 'child_process'
+import fs from 'fs'
+import os from 'os'
+import path from 'path'
 import { Main } from './Main'
 import { Toolbar } from './Toolbar'
 
@@ -6,15 +10,41 @@ import 'normalize.css/normalize.css'
 import '@blueprintjs/core/lib/css/blueprint.css'
 import { AddBoxDialog } from './AddBoxDialog'
 
-type Action = { type: 'closeAddBoxDialog' } | { type: 'openAddBoxDialog' }
+const ROOT_DIRECTORY = `${os.homedir()}/WebBoxes`
+const FIXED_INSTALLER_OPTIONS =
+  '--unattendedmodeui none --mode unattended  --disable-components varnish --base_password pass  --launch_cloud 0'
+const WPCONFIG_NEEDLE =
+  '// ** MySQL settings - You can get this info from your web host ** //'
+const WPCONFIG_REPLACEMENT = `/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true);
+
+// ** MySQL settings - You can get this info from your web host ** //
+`
+type Action =
+  | { type: 'closeAddBoxDialog' }
+  | { type: 'openAddBoxDialog' }
+  | { type: 'updateBoxes' }
 
 interface Box {
   name: string
 }
 
 interface State {
-  isOpenAddBoxDialog: boolean
   boxes: Box[]
+  isOpenAddBoxDialog: boolean
+  isStale: boolean
 }
 
 const BOXES: Box[] = [
@@ -24,7 +54,11 @@ const BOXES: Box[] = [
   { name: 'probao' },
 ]
 
-const initialState = { isOpenAddBoxDialog: false, boxes: BOXES }
+const initialState = {
+  boxes: getBoxes(),
+  isOpenAddBoxDialog: false,
+  isStale: false,
+}
 
 function reducer(state: State, action: Action) {
   switch (action.type) {
@@ -38,6 +72,11 @@ function reducer(state: State, action: Action) {
         ...state,
         isOpenAddBoxDialog: false,
       }
+    case 'updateBoxes':
+      return {
+        ...state,
+        boxes: getBoxes(),
+      }
     default:
       state
   }
@@ -45,14 +84,15 @@ function reducer(state: State, action: Action) {
 
 export function App() {
   const [state, dispatch] = React.useReducer(reducer, initialState)
+
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <Toolbar onAddBox={openAddBoxDialog} />
-      <Main boxes={BOXES} />
+      <Main boxes={state.boxes} />
       <AddBoxDialog
         isOpen={state.isOpenAddBoxDialog}
         onClose={closeAddBoxDialog}
-        onCreate={(name: string) => console.log('Create Box: ' + name)}
+        onCreate={onCreateBox}
       />
     </div>
   )
@@ -61,7 +101,76 @@ export function App() {
     dispatch({ type: 'closeAddBoxDialog' })
   }
 
+  function onCreateBox(name: string) {
+    createBox(name, state.boxes)
+    dispatch({ type: 'updateBoxes' })
+  }
+
+  function createBox(name: string, boxes: Box[]) {
+    try {
+      // Create box directory
+      const bitnamiDirectory = path.normalize(
+        `${ROOT_DIRECTORY}/${name}/bitnami`
+      )
+      fs.mkdirSync(bitnamiDirectory, { recursive: true })
+
+      // install wordpress box
+      const wordpressInstaller = path.normalize(
+        `${ROOT_DIRECTORY}/.installers/bitnami-wordpress-installer.run`
+      )
+      const installerCommand = `${wordpressInstaller} --prefix ${bitnamiDirectory} --wordpress_blog_name ${name} --apache_server_port ${
+        8080 + boxes.length
+      } --mysql_port ${3306 + boxes.length} ${FIXED_INSTALLER_OPTIONS}`
+      child_process.execSync(installerCommand)
+
+      // config root directory
+      const bnConfig = path.normalize(
+        `${bitnamiDirectory}/apps/wordpress/bnconfig`
+      )
+      const bnConfigCommand = `${bnConfig} --appurl /`
+      child_process.execSync(bnConfigCommand)
+
+      // disable opcache
+      const phpIni = path.normalize(`${bitnamiDirectory}/php/etc/php.ini`)
+      const phpIniData = fs.readFileSync(phpIni, 'utf-8')
+      const newPhpIniData = phpIniData.replace(
+        'opcache.enable=1',
+        'opcache.enable=0'
+      )
+      fs.writeFileSync(phpIni, newPhpIniData, 'utf-8')
+
+      // enable debug output
+      const wpConfig = path.normalize(
+        `${bitnamiDirectory}/apps/wordpress/htdocs/wp-config.php`
+      )
+      const wpConfigData = fs.readFileSync(wpConfig, 'utf-8')
+      const newWpConfigData = wpConfigData.replace(
+        WPCONFIG_NEEDLE,
+        WPCONFIG_REPLACEMENT
+      )
+      fs.writeFileSync(wpConfig, newWpConfigData, 'utf-8')
+
+      // link wp-content to app directory
+      const wpContent = path.normalize(
+        `${bitnamiDirectory}/apps/wordpress/htdocs/wp-content`
+      )
+      const app = path.normalize(`${bitnamiDirectory}/../app`)
+      child_process.execSync(`ln -s ${wpContent} ${app}`)
+    } catch (e) {
+      alert(e)
+    } finally {
+      closeAddBoxDialog()
+    }
+  }
+
   function openAddBoxDialog() {
     dispatch({ type: 'openAddBoxDialog' })
   }
+}
+
+function getBoxes(): Box[] {
+  return fs
+    .readdirSync(ROOT_DIRECTORY)
+    .filter((el) => !el.startsWith('.'))
+    .map((el) => ({ name: el }))
 }
